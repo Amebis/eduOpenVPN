2fbf7d8f744f378e9e569ba108b8b19da1e9e0a6
Simon Rozman <simon@rozman.si>
2017-08-09 12:56:55 +0200

    eduOpenVPN.Tests project introduced

---
+++ eduOpenVPN.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿/*
+eduOpenVPN - An OpenVPN Client for eduVPN (and beyond)
+
+Copyright: 2017, The Commons Conservancy eduVPN Programme
+SPDX-License-Identifier: GPL-3.0+
+*/
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("eduOpenVPN.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Amebis")]
+[assembly: AssemblyProduct("eduOpenVPN")]
+[assembly: AssemblyCopyright("Copyright © Amebis 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("08f55aa7-91a7-4ba1-9b62-79053f9ea30d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
