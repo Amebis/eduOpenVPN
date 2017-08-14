32af3c818471bfbd7b87085f3e2334c9b7649919
Simon Rozman <simon@rozman.si>
2017-08-14 11:43:24 +0200

    Configuration helper class introduced

---
+++ eduOpenVPN/Configuration.cs
@@ -0,0 +1,20 @@
+﻿/*
+eduOpenVPN - An OpenVPN Client for eduVPN (and beyond)
+
+Copyright: 2017, The Commons Conservancy eduVPN Programme
+SPDX-License-Identifier: GPL-3.0+
+*/
+
+namespace eduOpenVPN
+{
+    public class Configuration
+    {
+
+        public static string EscapeParamValue(string value)
+        {
+            return value.IndexOfAny(new char[] { '\\', ' ', '"' }) >= 0 ?
+                "\"" + value.Replace("\\", "\\\\").Replace("\"", "\\\"") + "\"" : // Escape backslash and double quotes, and add surrounding quotes
+                value; // No need to escape
+        }
+    }
+}
