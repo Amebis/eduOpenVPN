c9dcd3f802f04395448f8644b88afb61717d6c1a
Simon Rozman <simon@rozman.si>
2017-11-27 09:06:22 +0100

    Discontinue AssemblyInformationalVersion

--- eduOpenVPN.Tests/Properties/AssemblyInfo.cs
+++ eduOpenVPN.Tests/Properties/AssemblyInfo.cs
@@ -31,4 +31,3 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 [assembly: AssemblyVersion("1.0.6")]
-[assembly: AssemblyInformationalVersion("1.0.6")]
--- eduOpenVPN/Properties/AssemblyInfo.cs
+++ eduOpenVPN/Properties/AssemblyInfo.cs
@@ -33,4 +33,3 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 [assembly: AssemblyVersion("1.0.6")]
-[assembly: AssemblyInformationalVersion("1.0.6")]
